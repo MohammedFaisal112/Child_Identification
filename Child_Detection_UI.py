e3de2a908bacdca04e4663ec52bd319cec12a1f5
Mohammed Faisal <165476571+MohammedFaisal112@users.noreply.github.com>
2024-05-23 16:10:08 +0530

    Add files via upload

---
+++ Child_Detection_UI.py
@@ -0,0 +1,71 @@
+# Importing Necessary Libraries
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import Image,ImageTk
+import numpy
+import numpy as np
+
+# Loading the Model
+from keras.models import load_model
+model=load_model('Age_Model.h5')
+
+# Initializing the GUI
+top=tk.Tk()
+top.geometry('800x600')
+top.title('Child Detector')
+top.configure(background='#CDCDCD')
+
+# Initializing the Labels (1 for age)
+label=Label(top,background="#CDCDCD",font=('arial',15,"bold"))
+sign_image=Label(top)
+
+# Definig Detect fuction which detects the age of the person in image using the model.
+def Detect(file_path):
+    global label_packed
+    image=Image.open(file_path)
+    image=image.resize((48,48))
+    image=numpy.expand_dims(image,axis=0)
+    image=np.array(image)
+    image=np.delete(image,0,1)
+    image=np.resize(image,(48,48,3))
+    print (image.shape)
+    image=np.array([image])/255
+    pred=model.predict(image)
+    age=int(np.round(pred))
+    if age<=20:
+        Detected = "It's a Child."
+    else:
+        Detected = "It's not a Child."
+    label.configure(foreground="#011638",text=Detected)
+
+# Defining Show_detect button function
+def show_Detect_button(file_path):
+    Detect_b=Button(top,text="Detect Image",command=lambda: Detect(file_path),padx=10,pady=5)
+    Detect_b.configure(background="#364156",foreground='white',font=('arial',10,'bold'))
+    Detect_b.place(relx=0.79,rely=0.46) 
+
+# Definig Upload Image Function
+def upload_image():
+    try:
+        file_path=filedialog.askopenfilename()
+        uploaded=Image.open(file_path)
+        uploaded.thumbnail(((top.winfo_width()/2.25),(top.winfo_height()/2.25)))
+        im=ImageTk.PhotoImage(uploaded)
+
+        sign_image.configure(image=im)
+        sign_image.image=im
+        label.configure(text='')
+        show_Detect_button(file_path)
+    except:
+        pass
+
+upload=Button(top,text="Upload an Image",command=upload_image,padx=10,pady=5)
+upload.configure(background="#364156",foreground='white',font=('arial',10,'bold'))
+upload.pack(side='bottom',pady=50)
+sign_image.pack(side='bottom',expand=True)
+label.pack(side="bottom",expand=True)
+heading=Label(top,text="Child Detector",pady=20,font=('arial',20,"bold"))
+heading.configure(background="#CDCDCD",foreground="#364156")
+heading.pack()
+top.mainloop()
